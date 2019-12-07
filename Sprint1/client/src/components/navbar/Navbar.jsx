0b6b022740bb98c45753f3859b1cccb0fd7686a8
Redwan Zia <redwan.zia@gmail.com>
2019-12-07 18:12:57 -0500

    added navigation

--- Sprint1/client/src/components/navbar/Navbar.jsx
+++ Sprint1/client/src/components/navbar/Navbar.jsx
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-import './navbar.scss'
-import  Logo from '../../asset/pinkLogo.png'
-
-
+import { Link } from 'react-router-dom';
+import './navbar.scss';
+import Logo from '../../asset/pinkLogo.png';
 
 class Nav extends Component {
-  state = {  }
-  render() { 
-    return ( 
-      <div className="nav">
-      <a href="http://localhost:3000/"><img className="nav__logo" src={Logo} alt=""></img></a>
-      </div>
-     );
-  }
+	state = {};
+	render() {
+		return (
+			<div className='nav'>
+				<input className='nav__checkbox' type='checkbox' id='nav-toggle' />
+				<label className='nav__button' htmlFor='nav-toggle'>
+					<span className='nav__icon'>&nbsp;</span>
+				</label>
+				<div className='nav__background'>&nbsp;</div>
+				<div className='nav__navigation'>
+					<ul className='nav__list'>
+						<li className='nav__item'>
+							{' '}
+							<a href='#' className='nav__link'>
+								{' '}
+								Login{' '}
+							</a>{' '}
+						</li>
+						<li className='nav__item'>
+							{' '}
+							<a href='#' className='nav__link'>
+								{' '}
+								About{' '}
+							</a>{' '}
+						</li>
+					</ul>
+				</div>
+				<a href='http://localhost:3000/'>
+					<img className='nav__logo' src={Logo} alt='' />
+				</a>
+			</div>
+		);
+	}
 }
- 
+
 export default Nav;
--- Sprint1/client/src/components/showCase/showCase.jsx
+++ Sprint1/client/src/components/showCase/showCase.jsx
@@ -1,51 +1,56 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
-import Show from '../../asset/showCase1.jpg'
-import './showCase.scss'
-import Jesseca from '../../asset/jesseca .jpg'
-import Henry from '../../asset/Henry.png'
-import main from '../../asset/imgStich-1.jpg'
-import Kaitlin from '../../asset/kaitlin .jpg'
-
-
-
+import { Link } from 'react-router-dom';
+import Show from '../../asset/showCase1.jpg';
+import './showCase.scss';
+import Jesseca from '../../asset/jesseca .jpg';
+import Henry from '../../asset/Henry.png';
+import main from '../../asset/imgStich-1.jpg';
+import Kaitlin from '../../asset/kaitlin .jpg';
 
 class Showcase extends Component {
-  state = {  }
-  render() { 
-    return ( 
-      <div className="showCase">
-      <figure className = 'showCase__shape'>
-      <img className='showCase__imgShape' src={main}/>
-      <p className='showCase__imgText'>“Cancer is just a <span className='showCase__span'>chapter</span> chapter in our lives and not the  <span className='showCase__span'>whole story</span>.”</p>    
-      </figure>
-        
- 
-        <div className="showCase__position">
-          <Link to='/research' className="showCase__research">Research</Link>
-          <Link to='/support' className="showCase__research showCase__med">Treatment Support</Link>
-          <Link to='/type' className="showCase__research showCase__kind">Information By Type</Link>          
-        </div> 
+	state = {};
+	render() {
+		return (
+			<div className='showCase'>
+				<figure className='showCase__shape'>
+					<img className='showCase__imgShape' src={main} />
+					<p className='showCase__imgText'>
+						“Cancer is just a <span className='showCase__span'>chapter</span> in our lives and not the{' '}
+						<span className='showCase__span'>whole story</span>.”
+					</p>
+				</figure>
 
-        <img className="showCase__img" src={Show} alt=""></img>  
+				<div className='showCase__position'>
+					<Link to='/research' className='showCase__research'>
+						Research
+					</Link>
+					<Link to='/support' className='showCase__research showCase__med'>
+						Treatment Support
+					</Link>
+					<Link to='/type' className='showCase__research showCase__kind'>
+						Information By Type
+					</Link>
+				</div>
 
-        <div className="showCase__quote" >
-          <div className="showCase__quote__wrap">
-            <p className="showCase__quote__text">“The best thing to do is take one day at a time, be positive, pick your head up when you are down and remember what may seem like the most devastating news in your life, can always open doors to a whole new world of possibilities.”</p>
-            <div className="showCase__quote__composition" >
-              <img className="showCase__quote__jesseca" src={Jesseca}></img>             
-              <img className="showCase__quote__henry" src={Henry} ></img>
-              <img className="showCase__quote__kaitlin" src={Kaitlin} ></img>
-            </div> 
-          </div> 
-        </div> 
+				<img className='showCase__img' src={Show} alt='' />
 
-
-        
-
-      </div>
-     );
-  }
+				<div className='showCase__quote'>
+					<div className='showCase__quote__wrap'>
+						<p className='showCase__quote__text'>
+							“The best thing to do is take one day at a time, be positive, pick your head up when you are
+							down and remember what may seem like the most devastating news in your life, can always open
+							doors to a whole new world of possibilities.”
+						</p>
+						<div className='showCase__quote__composition'>
+							<img className='showCase__quote__jesseca' src={Jesseca} />
+							<img className='showCase__quote__henry' src={Henry} />
+							<img className='showCase__quote__kaitlin' src={Kaitlin} />
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
- 
+
 export default Showcase;
